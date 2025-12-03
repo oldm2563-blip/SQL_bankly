d57c27748734fc0e885e47bb7fabb8969d386c9d
oldman <oldm2563@email.com>
2025-12-03 14:46:15 +0100

    accounts Table

--- db.sql
+++ db.sql
@@ -15,3 +15,14 @@ CREATE TABLE advisors(
     full_name varchar(255),
     email varchar(255),
 );
+
+CREATE TABLE accounts(
+    account_id int PRIMARY KEY AUTO_INCREMENT,
+    account_number int,
+    balance int,
+    account_type enum('Checking','Savings','Business'),
+    customer_id int,
+    FOREIGN KEY (customers_id) REFERENCES customers (customer_id),
+    advisors_id int,
+    FOREIGN KEY (advisors_id) REFERENCES advisors (advisors_id)
+);
