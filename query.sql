7416cea6c850a5aef3cee991bb7411abcba22b63
oldman <oldm2563@email.com>
2025-12-03 16:16:05 +0100

    show just the name and the email of the customers and show a accounts and show just account number

--- query.sql
+++ query.sql
@@ -7,3 +7,12 @@ UPDATE customers SET phone = "+212 73823764" WHERE customer_id = 4;
 
 --3/ show all customers
 SELECT * FROM customers;
+
+--4/ show customers full name and email
+SELECT full_name, email FROM customers;
+
+--5/ show all accounts
+SELECT * FROM accounts;
+
+--6/ Show only account_number
+SELECT account_number FROM accounts;
