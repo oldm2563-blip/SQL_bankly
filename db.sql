c7effaf592c29158200ecac961d44e0e1de3eae8
oldman <oldm2563@email.com>
2025-12-03 14:35:37 +0100

    customers Table

--- db.sql
+++ db.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE Bankly;
 
 USE Bankly;
+
+CREATE TABLE customers(
+    customer_id int PRIMARY KEY AUTO_INCREMENT,
+    full_name varchar(255),
+    email varchar(255),
+    phone varchar(20),
+    registration_date date 
+);
